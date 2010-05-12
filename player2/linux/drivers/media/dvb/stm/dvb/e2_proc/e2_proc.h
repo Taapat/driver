22531ab2ec00825eab81b6aebaa9c60c291b7a9b
spider <spworld2008@gmail.com>
2010-05-12 18:32:10 +0300

    [driver] add missing proc_avs_0_volume_write to e2_proc.h

--- player2/linux/drivers/media/dvb/stm/dvb/e2_proc/e2_proc.h
+++ player2/linux/drivers/media/dvb/stm/dvb/e2_proc/e2_proc.h
@@ -4,5 +4,6 @@
 int proc_video_aspect_get(void);
 int proc_video_policy_get(void);
 int e2_proc_audio_getPassthrough(void);
+int proc_avs_0_volume_write(struct file *file, const char __user *buf, unsigned long count, void *data);
 
 #endif
