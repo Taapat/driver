80275de6f670dbbc2743bc5c8c9d09ae30d9f770
babsy98 <babsy98@gmx.net>
2011-01-10 20:44:57 +0100

    [player179] sorry wrong version fix again mme :)

--- player2_179/player/codec/codec_mme_audio.cpp
+++ player2_179/player/codec/codec_mme_audio.cpp
@@ -716,7 +716,7 @@ MME_LxAudioDecoderInitParams_t &Params = AudioDecoderInitializationParameters;
 
 
     // Detect changes between BL025 and BL028 (delete this code when BL025 is accient history)
-#if DRV_MULTICOM_AUDIO_DECODER_VERSION > 0x090228
+#if DRV_MULTICOM_AUDIO_DECODER_VERSION >= 0x090128
 		Params.BlockWise.u32 = 0;
 #else
 		Params.BlockWise = ACC_MME_FALSE;
