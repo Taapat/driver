fc8af5a7eb175f475a03815140f078df0682d75b
konfetti <konfetti@gmx.net>
2011-01-30 15:01:38 +0100

    [octagon1008] stm23_123 2nd part

--- player2_179/linux/drivers/sound/pseudocard/pseudo_mixer.c
+++ player2_179/linux/drivers/sound/pseudocard/pseudo_mixer.c
@@ -200,7 +200,7 @@ static const struct snd_pseudo_mixer_downstream_topology default_topology[] = {
   #endif
 #else
 #if defined (CONFIG_KERNELVERSION)
-  #if defined(__TDT__) && ! defined(UFS922)
+  #if defined(__TDT__) && ! defined(UFS922)  && !defined(OCTAGON1008)
  			 CARD_SPDIF  ("SPDIF",   2, 0,  48000, 2),
       			 CARD        ("Analog",  1, 0,  48000, 2),
       			 CARD        ("HDMI",    0, 0,  48000, 2),
