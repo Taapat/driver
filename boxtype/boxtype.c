412b175aaed8ea5aafd335be136e859185526a43
oxygen-1 <rene.fuerst@live.com>
2011-01-31 21:58:50 +0100

    fix last commit

--- boxtype/boxtype.c
+++ boxtype/boxtype.c
@@ -138,7 +138,10 @@ int __init boxtype_init(void)
 void __exit boxtype_exit(void)
 {
 	dprintk("[BOXTYPE] unloading ...\n");
-	//driver_unregister (&boxtype_driver);
+// TODO: FIX THIS
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,30)
+	driver_unregister (&boxtype_driver);
+#endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,26)
 	remove_proc_entry(procfs_name, &proc_root);
 #else
