68f6d5b01c52281dcb99ec4b7ccbee8de3023e30
konfetti <konfetti@gmx.net>
2011-11-01 23:45:28 +0100

    [hs7810a] lnb fix (thx spider)

--- frontends/lnb/a8293.c
+++ frontends/lnb/a8293.c
@@ -39,11 +39,7 @@ unsigned char a8293_read(struct i2c_client *client)
 
 int a8293_write(struct i2c_client *client, unsigned char reg)
 {
-	unsigned char byte;
-
-	dprintk("[A8293]: %s >\n", __func__);
-
-	byte = 0;
+	dprintk("[A8293]: 0x%02x %s >\n", reg, __func__);
 
 	if ( 1 != i2c_master_send(client, &reg, 1))
 	{
@@ -59,7 +55,7 @@ int a8293_write(struct i2c_client *client, unsigned char reg)
 int a8293_command_kernel(struct i2c_client *client, unsigned int cmd, void *arg )
 {
 	unsigned char reg = 0x10;
-
+    
 	dprintk("[A8293]: %s (%x)\n", __func__, cmd);
 
 	if(cmd != LNB_VOLTAGE_OFF)
@@ -69,6 +65,7 @@ int a8293_command_kernel(struct i2c_client *client, unsigned int cmd, void *arg 
 	{
 		case LNB_VOLTAGE_OFF:
 			dprintk("[A8293]: set voltage off\n");
+
 			return a8293_write(client, reg);
 
 		case LNB_VOLTAGE_VER:
@@ -93,8 +90,44 @@ int a8293_command(struct i2c_client *client, unsigned int cmd, void *arg)
 
 
 int a8293_init(struct i2c_client *client)
-{
-	return a8293_write(client, 0x82);
+{    
+	unsigned char reg;
+    int res;
+
+    /* this read is necessarily needed, otherwise
+     * lnb power is not supplied!
+     */	
+    reg = a8293_read(client);
+
+	dprintk("[A8293]: %s -> 0x%02X\n", __func__, reg);
+
+    res = a8293_write(client, 0x82);
+    
+    if (res == 0)
+    {
+       /* setup pio6 */
+       u32 reg = ctrl_inl(0xfd026030);
+
+       printk("%s: reg = 0x%08x\n", __func__, reg);
+
+       reg |= 0x00000001;
+
+       printk("%s: reg = 0x%08x\n", __func__, reg);
+              
+       ctrl_outl(reg, 0xfd026030);
+       
+       reg = ctrl_inl(0xfd026000);
+       
+       printk("%s: reg = 0x%08x\n", __func__, reg);
+
+       reg &= ~(0x0000001);
+
+       printk("%s: reg = 0x%08x\n", __func__, reg);
+
+       ctrl_outl(reg, 0xfd026000);
+    }
+
+	return res;
 }
 
 /* ---------------------------------------------------------------------- */
