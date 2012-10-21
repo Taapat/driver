b53cf695465ddb81d78abd3bbb689354d8b36af9
schpuntik <schpuntik@freenet.de>
2012-10-22 01:52:04 +0200

    remove ADB_BOX

--- player2_191/player/manifestor/manifestor_video.cpp
+++ player2_191/player/manifestor/manifestor_video.cpp
@@ -1274,7 +1274,6 @@ ManifestorStatus_t Manifestor_Video_c::SetDisplayWindows (struct VideoDisplayPar
 
     // Hm why is here not the decimate value used from havana_stream ?
     // Lets set it depending on the value from havana_stream
-#if !defined(ADB_BOX)
 	if(OutputWindow.Height > 576 || OutputWindow.Height < 425)
     {
         int decimate = Player->PolicyValue (Playback, Stream, PolicyDecimateDecoderOutput);
@@ -1283,14 +1282,6 @@ ManifestorStatus_t Manifestor_Video_c::SetDisplayWindows (struct VideoDisplayPar
         else
             DecimateIfAvailable = true;
     }
-#endif
-#if defined(ADB_BOX)
-#if 0	//1== bardzo zly obraz na SD 0== obraz ok
-	int decimate = Player->PolicyValue (Playback, Stream, PolicyDecimateDecoderOutput);
-    if (decimate == PolicyValueDecimateDecoderOutputDisabled) DecimateIfAvailable = false;
-    else DecimateIfAvailable = true;
-#endif
-#endif
 
 #if defined (CROP_INPUT_WHEN_DECIMATION_NEEDED_BUT_NOT_AVAILABLE)
     if ((Player->PolicyValue (Playback, Stream, PolicyDecimateDecoderOutput) != PolicyValueDecimateDecoderOutputDisabled) &&
