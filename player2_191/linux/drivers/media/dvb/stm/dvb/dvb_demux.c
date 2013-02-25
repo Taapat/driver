afd360bf25651ea48b3ae32bf9b70855cc9d0e50
schpuntik <schpuntik@freenet.de>
2013-02-25 22:47:40 +0100

    [libeplayer3_191] fix recoding freezer on tuner0 and demux1/2 or tuner1 and demux0/2 or tuner3 and demux1/2  thx obi

--- player2_191/linux/drivers/media/dvb/stm/dvb/dvb_demux.c
+++ player2_191/linux/drivers/media/dvb/stm/dvb/dvb_demux.c
@@ -226,12 +226,30 @@ int StartFeed (struct dvb_demux_feed* Feed)
     DVB_DEBUG ("(demux%d)\n", Context->Id);
 
     /* either numRunningFeeds == 0 and reset_tsm == 1 or reset_tsm > 1 */
+#ifdef __TDT__
+	// fix recoding freezer on tuner0 and demux1/2 or tuner1 and demux0/2 or tuner2 and demux0/1
+	int tsm_reset = 1;
+	
+	for (i = 0; i < DVB_MAX_DEVICES_PER_ADAPTER; i++)
+	{
+	    struct DeviceContext_s* DeviceContext = &DvbContext->DeviceContext[i];
+	    if(DeviceContext->numRunningFeeds != 0)
+	    	tsm_reset = 0;
+	}
+	
+	if (tsm_reset && reset_tsm)
+	{
+	    printk(KERN_WARNING "reset_tsm: %d numRunningFeeds: %d => calling stm_tsm_init(1)\n", reset_tsm, Context->numRunningFeeds);
+	    stm_tsm_init(1);
+	}
+#else
     if (Context->numRunningFeeds == 0 && reset_tsm)
     {
         printk(KERN_WARNING "reset_tsm: %d numRunningFeeds: %d => calling stm_tsm_init(1)\n", reset_tsm, Context->numRunningFeeds);
         stm_tsm_init(1);
     }
 
+#endif
 
 #ifdef __TDT__
 #ifdef no_subtitles
